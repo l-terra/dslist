a90d9a89ef62e2822c50442a01160137f42c0a16
Lucas Terra <lucas.terra8587@gmail.com>
2025-08-21 20:59:41 -0300

    SQL, projection, GET games by list

---
+++ src/main/java/com/lucasterra/project/projections/GameMinProjection.java
@@ -0,0 +1,11 @@
+package com.lucasterra.project.projections;
+
+public interface GameMinProjection {
+
+    Long getId();
+    String getTitle();
+    Integer getYear();
+    String getImgUrl();
+    String getShortDescription();
+    Integer getPosition();
+}
--- src/main/java/com/lucasterra/project/services/GameService.java
+++ src/main/java/com/lucasterra/project/services/GameService.java
@@ -1,8 +1,10 @@
 package com.lucasterra.project.services;
 
 import com.lucasterra.project.dto.GameDTO;
+import com.lucasterra.project.dto.GameListDTO;
 import com.lucasterra.project.dto.GameMinDTO;
 import com.lucasterra.project.entities.Game;
+import com.lucasterra.project.projections.GameMinProjection;
 import com.lucasterra.project.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,6 +27,12 @@ public class GameService {
     @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
         return result.stream().map(x -> new GameMinDTO(x)).toList();
     }
 }
